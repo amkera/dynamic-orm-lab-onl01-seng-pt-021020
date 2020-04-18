f1ec88abbe92933e46089b563a8a36ac3c2cc5e3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-18 00:33:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,6 +58,7 @@ class InteractiveRecord
     self.class.column_names.each do |column_name|
       values << "'#{send(column_name)}'" unless send(column_name).nil?
     end 
+    #The above code, however, will result in a values array
     values.join(", ")
     binding.pry
   end 
