cf3af24a657b961795353d9365f48ca333e446fb
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-18 00:43:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -73,8 +73,8 @@ class InteractiveRecord
   end 
     
   def self.find_by_name(student_name)
-    sql = <<-SQL
-      "SELECT * FROM #{self.table_name} WHERE student_name = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE student_name = ?"
+    DB[:conn].execute(sql, student_name)
  
     
     
